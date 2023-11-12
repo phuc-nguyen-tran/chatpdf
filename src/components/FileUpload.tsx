92c64d46d9f50a8f7c1e08d00d713b5494371186
nguy3ntrann <phucnguyentran79@gmail.com>
2023-11-12 17:04:18 -0500

    first commit

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { uploadToS3 } from "@/lib/s3";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { Inbox, Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useDropzone } from "react-dropzone";
+import { toast } from "react-hot-toast";
+
+type Props = {};
+
+const FileUpload = (props: Props) => {
+  const router = useRouter();
+  const [uploading, setUploading] = React.useState(false);
+  const { mutate } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/create-chat", {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "application/pdf": [".pdf"] },
+    maxFiles: 1,
+    onDrop: async (acceptedFiles) => {
+      console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        //bigger than 10mb
+        toast.error("file too large");
+        return;
+      }
+
+      try {
+        setUploading(true);
+        const data = await uploadToS3(file);
+        if (!data?.file_key || !data?.file_name) {
+          toast.error("something went wrong");
+          return;
+        }
+        mutate(data, {
+          onSuccess: ({chat_id}) => {
+            // console.log(data);
+            toast.success("Chat created");
+            router.push(`/chat/${chat_id}`);
+          },
+          onError: (error) => {
+            toast.error("error creating chat");
+            console.error(error);
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setUploading(false);
+      }
+    },
+  });
+  return (
+    <div className="p-2 bg-white rounded-xl">
+      <div
+        {...getRootProps({
+          className:
+            "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
+        })}
+      >
+        {uploading ? (
+          <>
+            {/* loading state */}
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">
+              Spilling Tea to GPT...
+            </p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FileUpload;
